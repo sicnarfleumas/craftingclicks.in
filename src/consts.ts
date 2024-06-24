72e5aed71a0acd2b27c403e39e5a7aabd67631f0
Samuel Francis <samuel.francis@learner.manipal.edu>
2024-06-24 20:06:12 +0530

    Full reset

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Crafting Clicks - We build the best websites';
-export const SITE_DESCRIPTION = 'We specialize in crafting SEO-friendly, fast, engaging websites that not only look great but also rank high.';
