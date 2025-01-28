48a44a3a8b8e5a3c81e7fbdc8b3f72060b6756f5
Samuel Francis <samuel.francis@learner.manipal.edu>
2025-01-28 21:11:06 +0530

    canonicals set to craftingclicks.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.craftingclicks.in',
+  site: 'https://craftingclicks.com/',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
